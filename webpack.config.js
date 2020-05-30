9a4a490f7c198d6f2874dac71fcbba39e095307f
jakeloew <jakeloew1@gmail.com>
2020-05-30 11:00:12 -0600

    adds Procfile for deployment

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  mode: 'development',
+  mode: process.env.NODE_ENV || 'development',
   entry: './app/index.js',
   output: {
     path: path.resolve(__dirname, 'public'),
